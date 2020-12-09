13300bea81148ba525a38e4816329300d9820a20
FernandoVazZ <fernandolfv97@gmail.com>
2020-12-09 11:19:16 -0600

    workspaceDidChange

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './customBlocks/custom_Blocks'
 import React from 'react'
 import ReactBlockly from 'react-blockly'
+import Blockly from 'blockly';
 
 export default function App() {
   const initialXml = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
@@ -43,19 +44,33 @@ export default function App() {
       ]
     }
   ]
+  function workspaceDidChange(workspace) {
+    const newXml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+    document.getElementById('generated-xml').innerText = newXml;
+
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+    document.getElementById('code').value = code;
+  }
+
   return (
-    <ReactBlockly
-      toolboxCategories={toolboxCategories}
-      initialXml={initialXml}
-      wrapperDivClassName="fill-height"
-      workspaceConfiguration={{
-        grid: {
-          spacing: 20,
-          length: 3,
-          colour: '#ccc',
-          snap: true,
-        },
-      }}
-    />
+    <>
+      <ReactBlockly
+        toolboxCategories={toolboxCategories}
+        initialXml={initialXml}
+        wrapperDivClassName="fill-height"
+        workspaceConfiguration={{
+          grid: {
+            spacing: 20,
+            length: 3,
+            colour: '#ccc',
+            snap: true,
+          },
+        }}
+        workspaceDidChange={workspaceDidChange}
+      />
+      <pre id="generated-xml">
+      </pre>
+      <textarea id="code" style={{ height: "200px", width: "400px" }} value=""></textarea>
+    </>
   )
 }
